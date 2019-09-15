feb73da4d857faf1b0be999aba2432fedbdfb801
Devin <devinwreeks@gmail.com>
2019-09-15 16:31:06 +1000

    testing accelerometer

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 let accelerometer = new Accelerometer({frequency: 60});
 
 accelerometer.addEventListener('reading', e => {
-  console.log("Acceleration along the X-axis " + accelerometer.x);
-  console.log("Acceleration along the Y-axis " + accelerometer.y);
-  console.log("Acceleration along the Z-axis " + accelerometer.z);
+    document.getElementById("xaxe").innerHTML = accelerometer.x
+    document.getElementById("yaxe").innerHTML = accelerometer.y
+    document.getElementById("zaxe").innerHTML = accelerometer.z
+//   console.log("Acceleration along the X-axis " + accelerometer.x);
+//   console.log("Acceleration along the Y-axis " + accelerometer.y);
+//   console.log("Acceleration along the Z-axis " + accelerometer.z);
 });
 accelerometer.start();
